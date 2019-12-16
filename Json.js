c251347e0d28c0e93d03a97cf6b3059e79ad20a8
frontendgod <3054750147@qq.com>
2019-12-17 00:09:49 +0800

    订单页面

--- Json.js
+++ Json.js
@@ -77,7 +77,52 @@ const billboardList = [
 	}
 ]
 
+/*订单列表*/
+const orderList = [
+	{
+		id: 1,
+		img: "/static/logo.png",
+		title: '程新林演唱会门票',
+		detail: '详情相亲相亲',
+		status: 1,
+		type: 0,
+	},
+	{
+		id: 2,
+		img: "/static/logo.png",
+		title: '程新林演唱会门票',
+		detail: '详情相亲相亲',
+		status: 0,
+		type: 0,
+	},
+	{
+		id: 3,
+		img: "/static/logo.png",
+		title: '程新林演唱会门票',
+		detail: '详情相亲相亲',
+		status: 0,
+		type: 2,
+	},
+	{
+		id: 4,
+		img: "/static/logo.png",
+		title: '程新林演唱会门票',
+		detail: '详情相亲相亲',
+		status: 0,
+		type: 2,
+	},
+	{
+		id: 5,
+		img: "/static/logo.png",
+		title: '程新林演唱会门票',
+		detail: '详情相亲相亲',
+		status: 2,
+		type: 2,
+	},
+]
+
 export default {
 	carouselList,
-	billboardList
+	billboardList,
+	orderList
 }
