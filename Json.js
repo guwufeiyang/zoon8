dfc5a3a5fd849cada9394f2942f07cb5bf595f13
ssdutliuhaibo <ssdutliuhaibo@126.com>
2019-12-18 18:02:18 +0800

    去掉测试数据

--- Json.js
+++ Json.js
@@ -51,74 +51,6 @@ const contributeAllList = [{
 }
 ]
 
-/* 粉丝团 */
-const fanlist = [
-{ 
-	fanId: 1,
-	integral: 18888,
-	name: 'Guwufeiyang',
-	portrait: '/static/temp/portrait-img1.jpg',
-	message: '欢迎大家来到这里。',
-	time: "14:09:31"
-}, {
-	fanId: 2,
-	integral: 18882342,
-	name: '摩托姐姐',
-	portrait: '/static/temp/portrait-img2.jpg',
-	message: '甜甜加油啊！',
-	time: "14:09:31"
-}, {
-	fanId: 3,
-	integral: 188883,
-	name: '最爱甜甜了',
-	portrait: '/static/temp/portrait-img3.jpg',
-	message: '小摩托一起加油！',
-	time: "14:09:31"
-},
-{ 
-	fanId: 4,
-	integral: 1823224,
-	name: 'Guwufeiyang',
-	portrait: '/static/temp/portrait-img1.jpg',
-	message: '欢迎大家来到这里。',
-	time: "14:09:31"
-}, {
-	fanId: 5,
-	integral: 18882342,
-	name: '摩托姐姐',
-	portrait: '/static/temp/portrait-img2.jpg',
-	message: '甜甜加油啊！',
-	time: "14:09:31"
-}, {
-	fanId: 6,
-	integral: 188883,
-	name: '最爱甜甜了',
-	portrait: '/static/temp/portrait-img3.jpg',
-	message: '小摩托一起加油！',
-	time: "14:09:31"
-}, { 
-	fanId: 4,
-	integral: 18888888888,
-	name: 'Guwufeiyang',
-	portrait: '/static/temp/portrait-img1.jpg',
-	message: '欢迎大家来到这里。',
-	time: "14:09:31"
-}, {
-	fanId: 5,
-	integral: 18882342,
-	name: '摩托姐姐',
-	portrait: '/static/temp/portrait-img2.jpg',
-	message: '甜甜加油啊！',
-	time: "14:09:31"
-}, {
-	fanId: 6,
-	integral: 188883,
-	name: '最爱甜甜了',
-	portrait: '/static/temp/portrait-img3.jpg',
-	message: '小摩托一起加油！',
-	time: "14:09:31"
-}]
-
 /* 贡献积分底部弹窗 */
 const propList = [
 	{
@@ -210,7 +142,6 @@ const orderList = [
 export default {
 	contributeList,
 	contributeAllList,
-	fanlist,
 	propList,
 	orderList
 }
