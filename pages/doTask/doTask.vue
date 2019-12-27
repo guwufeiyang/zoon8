89d90bc3c1fd82a2a93ee4a357a0c02c24d191ea
ssdutliuhaibo <ssdutliuhaibo@126.com>
2019-12-27 15:12:27 +0800

    同步

--- pages/doTask/doTask.vue
+++ pages/doTask/doTask.vue
@@ -25,9 +25,9 @@
 								</view>
 							</view>
 							<view class="task-r">
-								<image :src="(task.finished == 1 || task.count < task.threshold) ? '../../static/task-btn-disabled.png' : '../../static/task-btn.png'"
-									class="task-btn" @tap="manualConfirm(task.name)"></image>
-								<view class="task-txt" >{{task.finished == 1 ? '已完成' : task.count < task.threshold ? '待完成' : '领取'}}</view>
+								<image :src="(task.finished == 1) ? '../../static/task-btn-disabled.png' : '../../static/task-btn.png'"
+									class="task-btn" @tap="manualConfirm(task)"></image>
+								<view class="task-txt" >{{task.finished == 1 ? '已完成' : task.count < task.threshold ? '去完成' : '领取'}}</view>
 								<view class="has-finish">
 									已完成
 									<text class="progress">{{task.count}}/{{task.threshold}}</text>
@@ -37,101 +37,24 @@
 					</view>
 				</view>
 				<view class="tab-content-wrap" v-if="currentTab==='achievement'">
-					<view class="task-list">
+					<view class="task-list" v-for="(task, index) in tasks" :key="index">
 						<view class="task-bg"></view>
 						<view class="task-content">
 							<view class="task-l">
-								<image class="img" src="../../static/icon-sign.png"></image>
+								<image class="img" :src="task.image"></image>
 								<view class="task-con">
-									<view class="task-name">优秀新人</view>
-									<view class="task-reward">连续7日签到</view>
+									<view class="task-name">{{task.name}}</view>
+									<view class="task-reward">{{task.desc}}</view>
 								</view>
 							</view>
 							<view class="task-r">
-								<image src="../../static/task-btn.png" v-if="!userInfo.dailySign" class="task-btn" @tap="sign"></image>
-								<image src="../../static/task-btn-disabled.png" v-if="userInfo.dailySign"  class="task-btn"></image>
-								<view class="task-txt" >{{userInfo.dailySign ? '已完成' : '领取'}}</view>
+								<image :src=" task.count >= task.threshold ? '../../static/task-btn-disabled.png' : '../../static/task-btn.png'"
+								 class="task-btn" @tap="goFinish(task)"></image>
+								<view class="task-txt" >{{task.count >= task.threshold ? '已完成' : '去完成'}}</view>
 								<view class="has-finish">
 									已完成
-									<text class="progress">{{userInfo.dailySign ? "1" : "0" }}/1</text>
+									<text class="progress">{{task.count}}/{{task.threshold}}</text>
 								</view>
-							</view>
-						</view>
-					</view>
-					<view class="task-list">
-						<view class="task-bg"></view>
-						<view class="task-content">
-							<view class="task-l">
-								<image class="img" src="../../static/icon-fame.png"></image>
-								<view class="task-con">
-									<view class="task-name">小有名气</view>
-									<view class="task-reward">累计10次发言</view>
-								</view>
-							</view>
-							<view class="task-r">
-								<template>
-									<image src="../../static/task-btn-disabled.png" v-if="userInfo.dailyShareCount >= userInfo.shareTimes"  class="task-btn"></image>
-									<image src="../../static/task-btn.png" v-else class="task-btn"></image>
-								</template>
-								<view class="task-txt"> {{userInfo.dailyShareCount >= userInfo.shareTimes ? '已完成' : '待完成'}} </view>
-								<view class="has-finish">
-									已完成
-									<text class="progress">{{userInfo.dailyShareCount}}/{{userInfo.shareTimes}}</text>
-								</view>
-							</view>
-						</view>
-					</view>
-					<view class="task-list">
-						<view class="task-bg"></view>
-						<view class="task-content">
-							<view class="task-l">
-								<image class="img" src="../../static/icon-news.png"></image>
-								<view class="task-con">
-									<view class="task-name">新人粉丝</view>
-									<view class="task-reward">累计贡献666积分</view>
-								</view>
-							</view>
-							<view class="task-r">
-								<image src="../../static/task-btn.png" v-if="!userInfo.dailyLogin" class="task-btn"></image>
-								<image src="../../static/task-btn-disabled.png" v-if="userInfo.dailyLogin" class="task-btn"></image>
-								<view class="task-txt">已完成</view>
-								<view class="has-finish">已完成<text class="progress">1/1</text></view>
-							</view>
-						</view>
-					</view>
-					<view class="task-list">
-						<view class="task-bg"></view>
-						<view class="task-content">
-							<view class="task-l">
-								<image class="img" src="../../static/icon-sheep.png"></image>
-								<view class="task-con">
-									<view class="task-name">顺手牵羊</view>
-									<view class="task-reward">累计偷去666积分</view>
-								</view>
-							</view>
-							<view class="task-r">
-								<image src="../../static/task-btn.png" v-if="!userInfo.dailyLogin" class="task-btn"></image>
-								<image src="../../static/task-btn-disabled.png" v-if="userInfo.dailyLogin" class="task-btn"></image>
-								<view class="task-txt">已完成</view>
-								<view class="has-finish">已完成<text class="progress">1/1</text></view>
-							</view>
-						</view>
-					</view>
-					<view class="task-list">
-						<view class="task-bg"></view>
-						<view class="task-content">
-							<view class="task-l">
-								<image class="img" src="../../static/icon-downwind.png"></image>
-								<view class="task-con">
-									<view class="task-name">顺风顺水</view>
-									<view class="task-reward">累计抽奖获得666积分</view>
-								</view>
-							</view>
-							<view class="task-r">
-								<image src="../../static/task-btn.png" v-if="!userInfo.dailyLogin" class="task-btn"></image>
-								<image src="../../static/task-btn-disabled.png" v-if="userInfo.dailyLogin" class="task-btn"></image>
-								<view class="task-txt">已完成</view>
-								<view class="has-finish">已完成<text class="progress">1/1</text></view>
 							</view>
 						</view>
 					</view>
@@ -233,17 +156,28 @@
 				var meRes = await arequest('/me', null, {})
 				this.login(meRes.data.me || meRes.data)
 			},
-			async manualConfirm(taskName){
-				console.log(taskName)
-				var signRes = await arequest('/manualConfirm?task='+taskName, {}, {})
-
-				this.loadTabData()
+			async goFinish(task){
+				uni.navigateTo({
+					url: task.gotoUrl
+				})	
+			},
+			async manualConfirm(task){
+				if(task.count < task.threshold) {
+					uni.navigateTo({
+						url: task.gotoUrl
+					})				
+				} else {
+					var signRes = await arequest('/manualConfirm?task='+task.name, {}, {})
+					this.loadTabData()
+				}
 			},
 			async loadTabData() {
 				await this.reloadMe()
 				
-				var loadRes = await arequest('/loadTaskProcessRate?type='+this.currentTab, {}, {})
-				this.tasks = loadRes.data
+				if(this.currentTab == 'dailyTask' ||　this.currentTab == 'achievement') {
+					var loadRes = await arequest('/loadTaskProcessRate?type='+this.currentTab, {}, {})
+					this.tasks = loadRes.data
+				}
 			}
 		},
 		onShow() {
